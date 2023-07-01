f12c6235508b036bdb347b820bf17ae3aa543b12
HasegawaTomohiko <T.Hasegawa0603@gmail.com>
2023-07-02 00:51:42 +0900

    fix index.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter());
+app.use('/', indexRouter);
 app.use('/chat', chatRouter(iod));
 
 // catch 404 and forward to error handler
